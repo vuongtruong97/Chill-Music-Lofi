53f927e27b9f50146ac02976e74d02178f6abcca
Vương Trương <truongquocvuongnb@gmail.com>
2022-03-02 00:07:18 +0700

    day 2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+"use strict";
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const listOptionItem = $$(".option__item");
+const optionMenu = $(".option");
+window.onload = function () {
+  // Animate Icon Option Menu
+  optionMenu.onclick = function (e) {
+    e.preventDefault();
+    for (let item of listOptionItem) {
+      if (item.classList.contains("active"))
+        if (item === e.target.closest(".option__item")) {
+          continue;
+        }
+      item.classList.remove("active");
+    }
+    e.target.closest(".option__item").classList.toggle("active");
+  };
+};
