c6a7a5d658884c40c97f09dfdcb03b6d75f529ab
Vương Trương <truongquocvuongnb@gmail.com>
2022-03-02 23:57:35 +0700

    update day3

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,10 +10,22 @@ window.onload = function () {
     for (let item of listOptionItem) {
       if (item.classList.contains("active"))
         if (item === e.target.closest(".option__item")) {
-          continue;
+          //   continue;
         }
       item.classList.remove("active");
     }
     e.target.closest(".option__item").classList.toggle("active");
   };
+  // Input handle progress
+  {
+    const trafficInput = $("#city__traffic");
+    trafficInput.oninput = function () {
+      console.log(this.value);
+    };
+    const xxxx = $(".option__item__menu--set");
+    xxxx.onscroll = function (e) {
+      e.stopPropagation();
+      console.log(e);
+    };
+  }
 };
